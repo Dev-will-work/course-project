42094a0be8f49fc8b4cad972426108fb620f74d5
Pasha <paemshanov@edu.hse.ru>
2020-06-07 20:31:02 +0300

    Почта работает, отписка от рассылки - нет, вовлечение новых данных - нет

--- Курсовая/Курсовая.py
+++ Курсовая/Курсовая.py
@@ -3,12 +3,15 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib, ssl
 
+import psycopg2
+from psycopg2 import sql
+
 def Create_message(name, address):
     msg = MIMEMultipart("alternative")
-    link = "<a href='https://www.google.com/'>here</a>"
-    raw_link = 'here: https://www.google.com/'
-    message = "Hello, dear %s! Thank you for your subscription. You can see your QR-code below.<br>To unsubscribe, click %s" % (name, link)
-    raw = "Hello, dear %s! Thank you for your subscription. You can see your QR-code below.\nTo unsubscribe, click %s" % (name, link)
+    link = "<a href='https://www.google.com/'>здесь</a>"
+    raw_link = 'здесь: https://www.google.com/'
+    message = "Здравствуйте, %s! Благодарим вас за подписку на нашу рассылку.<br>Вы можете взять свой QR-код здесь.<br> Чтобы отписаться от рассылки нажмите %s" % (name, link)
+    raw = "Здравствуйте, %s! Благодарим вас за подписку на нашу рассылку.\n Вы можете взять свой QR-код здесь.\nЧтобы отписаться от рассылки нажмите %s" % (name, link)
     # setup the parameters of the message
     password = "StrCpPy24rh^"
     msg['From'] = "pshtest@mail.ru"
@@ -37,4 +40,50 @@ def Create_message(name, address):
  
         print("successfully sent email to %s:" % (msg['To']))
 
-Create_message("Pasha", "emshanov9@gmail.com")
+
+#def Connect(_dbname, _user, _password, _host):
+#    conn = psycopg2.connect(dbname=_dbname, user=_user, 
+#                        password=_password, host=_host)
+#    return conn
+
+#def Set_cursor(conn, cur):
+#    cur = conn.cursor()
+#    return cur
+
+    ##    with conn.cursor() as cursor:
+    ##    conn.autocommit = True
+    ##    values = [
+    ##        ('mom', 'mom', 'mom', '1999-01-08', 'M', 'six@nine.com', 'ololo', False, 5 )
+    ##        ]
+    ##    insert = sql.SQL('INSERT INTO users VALUES {}').format(sql.SQL(',').join(map(sql.Literal, values)))
+    ##    cursor.execute(insert)
+
+def Get_and_print_table(_dbname, _user, _password, _host): #То, что изначально было
+    conn = psycopg2.connect(dbname=_dbname, user=_user, 
+                        password=_password, host=_host)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM public.users LIMIT 5')
+    records = cursor.fetchall()
+    for row in cursor:
+        print(row)
+    cursor.close()
+    conn.close()
+
+def Change_db_user(_dbname, _user, _password, _host, username): #Проработало, но результат не дало, пока хз почему
+    conn = psycopg2.connect(dbname=_dbname, user=_user, 
+                        password=_password, host=_host)
+    cursor = conn.cursor()
+    cursor.execute('ALTER DATABASE %s OWNER TO %s' % (_dbname, username)) 
+    cursor.close()
+    conn.close()
+
+#def Close_connection(cursor):
+#    cursor.close()
+#    conn.close()
+
+Get_and_print_table('coursework', 'pasha', 'P4h0A0e0', 'eaplfm.com')
+#Create_message("Pasha", "emshanov9@gmail.com")
+#conn = Connect('coursework', 'pasha', 'P4h0A0e0', 'eaplfm.com')
+#cur = Set_cursor()
+#Change_db_user('coursework', 'pasha', 'P4h0A0e0', 'eaplfm.com', 'yarik')
+#Close_connection(cursor)
